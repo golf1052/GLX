6a47c25a4fa5693663493f991cc9bcd9b5d016f1
Sanders Lauture <golf1052@live.com>
2014-11-17 20:00:12 -0500

    A non sucky camera
    Beginning of camera smoothing stuff and draw everything using world
    class

--- GLX/Camera.cs
+++ GLX/Camera.cs
@@ -4,14 +4,69 @@ using System.Linq;
 using System.Text;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using System.Diagnostics;
 
 namespace GLX
 {
     public class Camera
     {
-        public Matrix transform;
-        public Matrix inverseTransform;
-        public Vector2 focalPoint;
+        private Matrix _transform;
+        internal Matrix transform
+        {
+            get
+            {
+                UpdateTransform();
+                return _transform;
+            }
+        }
+
+        private Matrix _inverseTransform;
+        public Matrix inverseTransform
+        {
+            get
+            {
+                return _inverseTransform;
+            }
+        }
+
+        public enum SmoothingType
+        {
+            Linear,
+            Smoothstep,
+            RecursiveLinear,
+            RecursiveSmoothstep,
+        }
+
+        public bool smoothPan;
+        public SmoothingType panSmoothingType;
+        public float panSmoothingRate;
+        private float panSmoothingValue;
+
+        private Vector2 _focalPoint;
+        public Vector2 focalPoint
+        {
+            get
+            {
+                return _focalPoint;
+            }
+            set
+            {
+                panSmoothingValue = 0;
+                if (smoothPan)
+                {
+                    startingFocalPoint = _focalPoint;
+                    targetFocalPoint = value;
+                }
+                else
+                {
+                    startingFocalPoint = value;
+                    _focalPoint = value;
+                    targetFocalPoint = value;
+                }
+            }
+        }
+        private Vector2 startingFocalPoint;
+        private Vector2 targetFocalPoint;
         public float zoom;
         public float rot;
         private Viewport viewport;
@@ -29,8 +84,14 @@ namespace GLX
 
         public Camera(Viewport viewport, Focus focus)
         {
+            smoothPan = false;
+            panSmoothingType = SmoothingType.Linear;
+            panSmoothingRate = 0.1f;
+            panSmoothingValue = 0;
             this.viewport = viewport;
             focalPoint = Vector2.Zero;
+            startingFocalPoint = Vector2.Zero;
+            targetFocalPoint = Vector2.Zero;
             smoothZoom = false;
             zoom = 1;
             targetZoom = 1;
@@ -39,26 +100,71 @@ namespace GLX
             this.focus = focus;
         }
 
-        public void Update()
+        void UpdateTransform()
         {
-            if (smoothZoom)
-            {
-                zoom = MathHelper.Lerp(zoom, targetZoom, zoomValue);
-            }
             if (focus == Focus.Center)
             {
-                transform = Matrix.CreateTranslation(new Vector3(-focalPoint.X, -focalPoint.Y, 0)) *
+                _transform = Matrix.CreateTranslation(new Vector3(-focalPoint.X, -focalPoint.Y, 0)) *
                     Matrix.CreateRotationZ(MathHelper.ToRadians(rot)) *
-                    Matrix.CreateScale(new Vector3(zoom, zoom, 0)) *
+                    Matrix.CreateScale(new Vector3(zoom, zoom, 1)) *
                     Matrix.CreateTranslation(new Vector3(viewport.Width * 0.5f, viewport.Height * 0.5f, 0));
             }
             else if (focus == Focus.TopLeft)
             {
-                transform = Matrix.CreateTranslation(new Vector3(-focalPoint.X, -focalPoint.Y, 0)) *
+                _transform = Matrix.CreateTranslation(new Vector3(-focalPoint.X, -focalPoint.Y, 0)) *
                     Matrix.CreateRotationZ(MathHelper.ToRadians(rot)) *
                     Matrix.CreateScale(new Vector3(zoom, zoom, 0));
             }
-            inverseTransform = Matrix.Invert(transform);
+            _inverseTransform = Matrix.Invert(_transform);
+        }
+
+        public void Update()
+        {
+            if (smoothPan)
+            {
+                if (_focalPoint != targetFocalPoint)
+                {
+                    if (panSmoothingType != SmoothingType.RecursiveLinear &&
+                        panSmoothingType != SmoothingType.RecursiveSmoothstep)
+                    {
+                        panSmoothingValue += panSmoothingRate;
+                        if (panSmoothingType == SmoothingType.Linear)
+                        {
+                            _focalPoint = Vector2.Lerp(startingFocalPoint, targetFocalPoint, panSmoothingValue);
+                        }
+                        else if (panSmoothingType == SmoothingType.Smoothstep)
+                        {
+                            _focalPoint = Vector2.SmoothStep(startingFocalPoint, targetFocalPoint, panSmoothingValue);
+                        }
+
+                        if (panSmoothingValue >= 1)
+                        {
+                            _focalPoint = targetFocalPoint;
+                            panSmoothingValue = 0;
+                        }
+                    }
+                    else
+                    {
+                        if (panSmoothingType == SmoothingType.RecursiveLinear)
+                        {
+                            _focalPoint = Vector2.Lerp(_focalPoint, targetFocalPoint, panSmoothingRate);
+                        }
+                        else if (panSmoothingType == SmoothingType.RecursiveSmoothstep)
+                        {
+                            _focalPoint = Vector2.SmoothStep(_focalPoint, targetFocalPoint, panSmoothingRate);
+                        }
+                    }
+                }
+                else
+                {
+                    panSmoothingValue = 0;
+                }
+            }
+            if (smoothZoom)
+            {
+                zoom = MathHelper.Lerp(zoom, targetZoom, zoomValue);
+            }
+            UpdateTransform();
         }
 
         public void CameraBeginSpriteBatch(SpriteBatch spriteBatch)
--- GLX/World.cs
+++ GLX/World.cs
@@ -10,18 +10,57 @@ namespace GLX
     public class World
     {
         GraphicsDeviceManager graphics;
-        public static int windowWidth;
-        public static int windowHeight;
+        SpriteBatch spriteBatch;
+
         public static Random random = new Random();
 
         List<GameTimeWrapper> gameTimes;
 
+        Dictionary<string, Camera> cameras;
+        private string _currentCamera;
+        public string currentCamera
+        {
+            get
+            {
+                return _currentCamera;
+            }
+            set
+            {
+                if (cameras.ContainsKey(value))
+                {
+                    _currentCamera = value;
+                }
+                else
+                {
+                    throw new Exception("That camera doesn't exist");
+                }
+            }
+        }
+
+        private Camera _camera1;
+        public Camera camera1
+        {
+            get
+            {
+                return _camera1;
+            }
+        }
+        public Vector2 cameraFocalPoint;
+
         public World(GraphicsDeviceManager graphics)
         {
             this.graphics = graphics;
-            windowWidth = graphics.PreferredBackBufferWidth;
-            windowHeight = graphics.PreferredBackBufferHeight;
             gameTimes = new List<GameTimeWrapper>();
+
+            cameras = new Dictionary<string, Camera>();
+            _camera1 = new Camera(graphics.GraphicsDevice.Viewport, Camera.Focus.TopLeft);
+            AddCamera("camera1", camera1);
+            _currentCamera = "camera1";
+        }
+
+        public void LoadSpriteBatch()
+        {
+            spriteBatch = new SpriteBatch(graphics.GraphicsDevice);
         }
 
         public void AddTime(GameTimeWrapper time)
@@ -29,12 +68,82 @@ namespace GLX
             gameTimes.Add(time);
         }
 
+        public bool AddCamera(string name, Camera camera)
+        {
+            if (cameras.ContainsKey(name))
+            {
+                return false;
+            }
+            else
+            {
+                cameras.Add(name, camera);
+                return true;
+            }
+        }
+
+        public bool RemoveCamera(string name)
+        {
+            if (name == "camera1")
+            {
+                throw new Exception("You cannot remove camera1");
+            }
+            else
+            {
+                if (cameras.ContainsKey(name))
+                {
+                    if (_currentCamera == name)
+                    {
+                        _currentCamera = "camera1";
+                    }
+                    cameras.Remove(name);
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
         public void Update(GameTime gameTime)
         {
+            cameras[currentCamera].Update();
             foreach (GameTimeWrapper time in gameTimes)
             {
                 time.Update(gameTime);
             }
+        }
+
+        public void BeginDraw()
+        {
+            BeginDraw(SpriteSortMode.Deferred, BlendState.AlphaBlend,
+                null, null, null, null,
+                cameras[currentCamera].transform);
+        }
+
+        public void BeginDraw(SpriteSortMode spriteSortMode, BlendState blendState)
+        {
+            BeginDraw(spriteSortMode, blendState,
+                null, null, null, null,
+                cameras[currentCamera].transform);
+        }
+
+        void BeginDraw(SpriteSortMode sortMode, BlendState blendState,
+            SamplerState samplerState, DepthStencilState depthStencilState,
+            RasterizerState rasterizerState, Effect effect, Matrix transformMatrix)
+        {
+            spriteBatch.Begin(sortMode, blendState, samplerState,
+                depthStencilState, rasterizerState, effect, transformMatrix);
+        }
+
+        public void Draw(Action<SpriteBatch> drawMethod)
+        {
+            drawMethod.Invoke(spriteBatch);
+        }
+
+        public void EndDraw()
+        {
+            spriteBatch.End();
         }
     }
 }
