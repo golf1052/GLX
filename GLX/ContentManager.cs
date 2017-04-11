3b3eb7404cd3e1bde0683783215851f63974c388
Sanders Lauture <golf1052@msn.com>
2017-04-11 00:57:11 -0400

    Add ContentManager class

---
+++ GLX/ContentManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GLX
+{
+    public class ContentManager<T>
+    {
+        private Microsoft.Xna.Framework.Content.ContentManager Content;
+        private Dictionary<string, T> loadedAssets;
+
+        public T this[string key]
+        {
+            get
+            {
+                if (loadedAssets.ContainsKey(key))
+                {
+                    return loadedAssets[key];
+                }
+                else
+                {
+                    throw new Exception("That asset hasn't been loaded.");
+                }
+            }
+        }
+
+        public ContentManager(Microsoft.Xna.Framework.Content.ContentManager Content)
+        {
+            this.Content = Content;
+            loadedAssets = new Dictionary<string, T>(StringComparer.OrdinalIgnoreCase);
+        }
+
+        public void Load(string assetName)
+        {
+            loadedAssets.Add(assetName, Content.Load<T>(assetName));
+        }
+    }
+}
